bca21430c76719c0bd4d8cf7bf61aedfe697a334
tsertic <tsertic5@gmail.com>
2023-08-31 14:14:10 +0200

    workit_langing_page: added typography,favicon

--- workit_landing_page/app/page.tsx
+++ workit_landing_page/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-eucaplyptus">eucalyptus</p>
-      <p className="text-dark-purple">dark-purple</p>
-      <p className="text-davys-grey">davys-gray</p>
+      <p className="text-eucaplyptus font-manrope font-normal">eucalyptus</p>
+      <p className="text-dark-purple font-manrope font-bold">dark-purple</p>
+      <p className="text-davys-grey font-fraunces">davys-gray</p>
+      <p className="text-davys-grey ">davys-gray</p>
       <p className="bg-davys-grey text-ghost-white">ghost-white</p>
       <p className="bg-davys-grey text-white">white</p>
     </main>
--- workit_landing_page/tailwind.config.ts
+++ workit_landing_page/tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      manrope: ["var(--font-manrope)"],
+      fraunces: ["var(--font-fraunces)"],
+    },
+    fontSize: {
+      body: ["18px", { lineHeight: "32px", fontWeight: "400" }],
+      button: ["18px", { lineHeight: "32px", fontWeight: "700" }],
+      headingL: ["80px", { lineHeight: "80px", fontWeight: "600" }],
+      headingM: ["56px", { lineHeight: "64px", fontWeight: "600" }],
+      headingS: ["32px", { lineHeight: "40px", fontWeight: "600" }],
+    },
     extend: {
       colors: {
         "dark-purple": "rgb(var(--color-dark-purple) / <alpha-value>)",
