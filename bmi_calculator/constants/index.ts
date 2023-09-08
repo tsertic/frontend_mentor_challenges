3b4b88767fa2eed668a64f7384137ab500e44df0
Tomislav Sertic <tsertic5@gmail.com>
2023-09-08 15:05:39 +0200

    bmi_calculator:added static data for tips and bmi limitations cards

--- bmi_calculator/constants/index.ts
+++ bmi_calculator/constants/index.ts
@@ -14,3 +14,49 @@ export const BMIResultsText = {
 };
 export const healthyBMILowerRange = 18.5;
 export const healthyBMIUpperRange = 24.9;
+
+export const tipsCards = [
+  {
+    iconUrl: "/images/icon-eating.svg",
+    title: "Healthy eating",
+    text: "Healthy eating promotes weight control, disease prevention, better digestion,immunity, mental clarity, and mood.",
+  },
+  {
+    iconUrl: "/images/icon-exercise.svg",
+    title: "Regular exercise",
+    text: "Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.",
+  },
+  {
+    iconUrl: "/images/icon-sleep.svg",
+    title: "Adequate sleep",
+    text: "Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.",
+  },
+];
+
+export const bmiLimitationsCards = [
+  {
+    iconUrl: "/images/icon-gender.svg",
+    title: "Gender",
+    text: "The development and body fat composition of girls and boys vary with age. Consequently,a child's age and gender are considered when evaluating their BMI.",
+  },
+  {
+    iconUrl: "/images/icon-age.svg",
+    title: "Age",
+    text: "In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content.",
+  },
+  {
+    iconUrl: "/images/icon-muscle.svg",
+    title: "Muscle",
+    text: "BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat.",
+  },
+  {
+    iconUrl: "/images/icon-pregnancy.svg",
+    title: "Pregnancy",
+    text: "Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child.",
+  },
+  {
+    iconUrl: "/images/icon-race.svg",
+    title: "Race",
+    text: "Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your  GP or practice nurse.",
+  },
+];
